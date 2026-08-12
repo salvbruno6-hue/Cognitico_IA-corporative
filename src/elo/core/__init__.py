ade731627c1f52e3d1769b1b3ab6c00eb11397cd
salvbruno6-hue <salvbruno6@gmail.com>
2026-08-12 16:17:20 -0300

    feat(core): expose canonical consulting contract

--- src/elo/core/__init__.py
+++ src/elo/core/__init__.py
@@ -1,6 +1,7 @@
 """Canonical ELO core boundaries."""
 
 from .canonical_identity import CanonicalIdentityRegistry, EloCanonicalIdentity
+from .consulting import ConsultingResponse, ConsultingStatus
 from .conversation_intake import ConversationEvent, ConversationIntake, ConversationIntakeResult
 from .evolution_memory import EvolutionMemory, EvolutionRecord
 from .knowledge_admission import AdmissionRequest, AdmissionResult, KnowledgeAdmission
@@ -9,6 +10,8 @@ __all__ = [
     "AdmissionRequest",
     "AdmissionResult",
     "CanonicalIdentityRegistry",
+    "ConsultingResponse",
+    "ConsultingStatus",
     "ConversationEvent",
     "ConversationIntake",
     "ConversationIntakeResult",
