c7aa3e3519d3d56c7348fa7c94acd9c1a64283ff
salvbruno6-hue <salvbruno6@gmail.com>
2026-08-14 06:29:43 -0300

    feat(core): expose scoped production and diagnostics contracts

--- src/elo/core/__init__.py
+++ src/elo/core/__init__.py
@@ -5,18 +5,21 @@ from .consulting import ConsultingResponse, ConsultingStatus
 from .conversation_bridge import ChatBridge, ChatBridgeEvent
 from .conversation_intake import ConversationEvent, ConversationIntake, ConversationIntakeResult
 from .context_resolution import ContextEvidence, ContextPack, ContextQuery, ContextResolutionEngine, ContextSource
+from .diagnostic_scenario_engine import DiagnosticLens, DiagnosticObservation, DiagnosticScenario, DiagnosticScenarioEngine, ScenarioMode
 from .evolution_memory import EvolutionMemory, EvolutionRecord
 from .gpt_handoff import GPTDecisionHandoff
 from .knowledge_admission import AdmissionRequest, AdmissionResult, KnowledgeAdmission
 from .maturity_engine import MATURITY_DIMENSIONS, MaturityAssessment
+from .production_flow import ProductionEvent, ProductionFlow, ProductionStage
 from .source_discovery import DiscoveryPlan, SourceCandidate, SourceDiscoveryEngine
 
 __all__ = [
     "AdmissionRequest", "AdmissionResult", "CanonicalIdentityRegistry", "ChatBridge",
     "ChatBridgeEvent", "ConsultingResponse", "ConsultingStatus", "ContextEvidence",
     "ContextPack", "ContextQuery", "ContextResolutionEngine", "ContextSource",
-    "ConversationEvent", "ConversationIntake", "ConversationIntakeResult", "DiscoveryPlan",
+    "ConversationEvent", "ConversationIntake", "ConversationIntakeResult", "DiagnosticLens",
+    "DiagnosticObservation", "DiagnosticScenario", "DiagnosticScenarioEngine", "DiscoveryPlan",
     "EloCanonicalIdentity", "EvolutionMemory", "EvolutionRecord", "GPTDecisionHandoff",
-    "KnowledgeAdmission", "MATURITY_DIMENSIONS", "MaturityAssessment", "SourceCandidate",
-    "SourceDiscoveryEngine",
+    "KnowledgeAdmission", "MATURITY_DIMENSIONS", "MaturityAssessment", "ProductionEvent",
+    "ProductionFlow", "ProductionStage", "ScenarioMode", "SourceCandidate", "SourceDiscoveryEngine",
 ]
