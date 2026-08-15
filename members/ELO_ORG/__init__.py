b6863211915f7cf61977af96a81c1d76e52c6a2f
salvbruno6-hue <salvbruno6@gmail.com>
2026-08-15 19:32:54 -0300

    fix: normalize ELO-ORG importable package name

---
+++ members/ELO_ORG/__init__.py
@@ -0,0 +1,21 @@
+"""ELO-ORG member capabilities."""
+
+from .corporate_systemic_model import (
+    AnalysisState,
+    CorporateFlow,
+    CorporateSystemicModel,
+    CrossDomainAnalysis,
+    DomainNode,
+    DomainRelation,
+    RelationKind,
+)
+
+__all__ = [
+    "AnalysisState",
+    "CorporateFlow",
+    "CorporateSystemicModel",
+    "CrossDomainAnalysis",
+    "DomainNode",
+    "DomainRelation",
+    "RelationKind",
+]
