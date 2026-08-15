45c08cf628ca5ce2dc099c6e52d2dbb60fecf01c
salvbruno6-hue <salvbruno6@gmail.com>
2026-08-14 21:33:24 -0300

    chore: export domain faculty primitives

---
+++ members/faculty/__init__.py
@@ -0,0 +1,21 @@
+"""Domain faculty primitives for governed member learning."""
+
+from .domain_faculty import (
+    ComparisonKind,
+    DomainFaculty,
+    DomainOverlay,
+    FacultyComparisonResult,
+    LogicStep,
+    build_overlay,
+    compare_faculty,
+)
+
+__all__ = [
+    "ComparisonKind",
+    "DomainFaculty",
+    "DomainOverlay",
+    "FacultyComparisonResult",
+    "LogicStep",
+    "build_overlay",
+    "compare_faculty",
+]
