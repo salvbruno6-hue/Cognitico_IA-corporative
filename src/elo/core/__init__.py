35e74df217a038021df6ac3e658acaac4908ff03
salvbruno6-hue <salvbruno6@gmail.com>
2026-08-18 11:43:09 -0300

    feat(core): expose systemic intelligence primitives

--- src/elo/core/__init__.py
+++ src/elo/core/__init__.py
@@ -40,6 +40,16 @@ from .maturity_engine import MATURITY_DIMENSIONS, MaturityAssessment
 from .production_flow import ProductionEvent, ProductionFlow, ProductionStage
 from .source_discovery import DiscoveryPlan, SourceCandidate, SourceDiscoveryEngine
 from .strategy_recovery import StrategicPath, StrategicRecoveryAssessment, assess_resolution_for_forward_strategy
+from .systemic_primitives import (
+    CausalAssessment,
+    DecisionRecord,
+    OutcomeFeedback,
+    Scenario,
+    SystemicModel,
+    SystemicRelation,
+    TemporalValidity,
+    UncertaintyAssessment,
+)
 
 __all__ = [
     "AdmissionRequest", "AdmissionResult", "Assumption", "BudgetAuthorization",
@@ -49,11 +59,13 @@ __all__ = [
     "BudgetingError", "CanonicalIdentityRegistry", "CapacityConstraint", "ChatBridge",
     "ChatBridgeEvent", "ConsultingResponse", "ConsultingStatus", "ContextEvidence", "ContextPack",
     "ContextQuery", "ContextResolutionEngine", "ContextSource", "ConversationEvent", "ConversationIntake",
-    "ConversationIntakeResult", "CostComponent", "DiagnosticLens", "DiagnosticObservation",
-    "DiagnosticScenario", "DiagnosticScenarioEngine", "DiscoveryPlan", "EloCanonicalIdentity",
-    "EvolutionMemory", "EvolutionRecord", "GPTDecisionHandoff", "GovernedBudgetingService",
-    "KnowledgeAdmission", "MATURITY_DIMENSIONS", "MaturityAssessment", "ProductionEvent", "ProductionFlow",
-    "ProductionStage", "ScenarioMode", "SourceCandidate", "SourceDiscoveryEngine",
-    "StrategicPath", "StrategicRecoveryAssessment", "assess_resolution_for_forward_strategy",
-    "SolicitationLearningCandidate", "build_learning_candidate", "classify_candidate",
+    "ConversationIntakeResult", "CostComponent", "CausalAssessment", "DecisionRecord", "DiagnosticLens",
+    "DiagnosticObservation", "DiagnosticScenario", "DiagnosticScenarioEngine", "DiscoveryPlan",
+    "EloCanonicalIdentity", "EvolutionMemory", "EvolutionRecord", "GPTDecisionHandoff",
+    "GovernedBudgetingService", "KnowledgeAdmission", "MATURITY_DIMENSIONS", "MaturityAssessment",
+    "OutcomeFeedback", "ProductionEvent", "ProductionFlow", "ProductionStage", "Scenario", "ScenarioMode",
+    "SourceCandidate", "SourceDiscoveryEngine", "StrategicPath", "StrategicRecoveryAssessment",
+    "SystemicModel", "SystemicRelation", "TemporalValidity", "UncertaintyAssessment",
+    "assess_resolution_for_forward_strategy", "SolicitationLearningCandidate", "build_learning_candidate",
+    "classify_candidate",
 ]
