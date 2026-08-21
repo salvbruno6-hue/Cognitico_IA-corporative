a935ec691ee1eeeb33a6db5e61c216dcc6f0e47c
salvbruno6-hue <salvbruno6@gmail.com>
2026-08-20 22:50:38 -0300

    feat(orcamento): initialize execution package

---
+++ src/elo/orcamento/__init__.py
@@ -0,0 +1,5 @@
+"""Canonical ELO budgeting execution package."""
+
+from .execution_engine import AuditFinding, BudgetExecution, BudgetExecutionEngine, BudgetItem
+
+__all__ = ["AuditFinding", "BudgetExecution", "BudgetExecutionEngine", "BudgetItem"]
