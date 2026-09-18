16fc0d7bbb76b844c09a3735a1d474133713ad7e
salvbruno6-hue <salvbruno6@gmail.com>
2026-09-18 09:50:47 -0300

    fix(data): preserve quotation view column compatibility

--- supabase/migrations/20260918095500_expose_cotacao_valor_base_por_metro.sql
+++ supabase/migrations/20260918095500_expose_cotacao_valor_base_por_metro.sql
@@ -8,12 +8,10 @@ select
   c.fornecedor_id,
   f.nome as fornecedor_nome,
   c.frete_total,
-  c.frete_rateio_metodo,
   c.icms_aliquota,
   c.icms_incluso,
   c.outros_tributos_aliquota,
   c.outros_tributos_inclusos,
-  c.status as status_cotacao,
   i.id as item_id,
   i.lista_mae_id,
   i.codigo_item,
@@ -24,16 +22,18 @@ select
   i.metros_totais,
   i.valor_unitario,
   i.valor_total,
-  case
-    when coalesce(i.metros_totais,0) > 0 then i.valor_total / i.metros_totais
-    else null
-  end as valor_base_por_metro,
   i.frete_rateado,
   i.tributos_adicionais,
   i.custo_total_com_encargos,
   i.custo_por_unidade_com_encargos,
   i.custo_por_metro,
-  i.status as status_item
+  i.status as status_item,
+  c.frete_rateio_metodo,
+  c.status as status_cotacao,
+  case
+    when coalesce(i.metros_totais,0) > 0 then i.valor_total / i.metros_totais
+    else null
+  end as valor_base_por_metro
 from public.fornecedor_cotacoes c
 join public.fornecedor_itens_cotacao i on i.cotacao_id=c.id
 join public.fornecedores f on f.id=c.fornecedor_id;
